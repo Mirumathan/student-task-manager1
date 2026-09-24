1d2d0660942b51c4eaeadc8b2a40256258de1a85
Mirumathan <mathanj923@gmail.com>
2026-09-24 11:54:32 +0530

    Add view tasks feature

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
-        System.out.print("Enter your task: ");
-        String task = sc.nextLine();
+        System.out.print("How many tasks do you want to add? ");
+        int n = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("Task added successfully!");
-        System.out.println("Your task: " + task);
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter task " + (i + 1) + ": ");
+            String task = sc.nextLine();
+            tasks.add(task);
+        }
+
+        System.out.println("\nYour Tasks:");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
 
         sc.close();
     }
